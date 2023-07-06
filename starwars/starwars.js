6d0d7621101e0840a15e41c5dface5975795bfb4
BThomas <bran_thom@yahoo.com>
2023-07-05 23:29:28 -0500

    movie section commented out

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -3,16 +3,57 @@
 
 async function get_api() {
     var number = document.getElementById("characterNumber").value
-        var response = await fetch(`https://www.swapi.tech/api/people/${number}`)
-        var data = await response.json();
-        console.log(data);
-        var character_name = data.result.properties.name
-        var eye_color = data.result.properties.eye_color
-        var h1 = document.getElementById("title");
-        var eye = document.getElementById("eye_color")
-        h1.innerText = "Character: " + character_name;
-        eye.innerText = "Eye color: " + eye_color
+    var people_url = await fetch(`https://www.swapi.tech/api/people/${number}`)
+    var data = await people_url.json();
+    console.log(data)
+    var character_name = data.result.properties.name
+    var eye_color = data.result.properties.eye_color
+    var h1 = document.getElementById("title");
+    var eye = document.getElementById("eye_color")
+    // var link = document.getElementById("link");
 
-        eye.style.color = eye_color;
-    
+    h1.innerText = "Character: " + character_name;
+    eye.innerText = "Eye color: " + eye_color
+
+    eye.style.color = eye_color;
+    get_movie(number)
 }
+
+async function get_movie(character_number) {
+    var response = await fetch("https://www.swapi.tech/api/films/")
+    var data = await response.json()
+    console.log(data.result)
+
+    var movies = data.result
+}
+//     var characterMovies = []
+
+//     for (var i = 0; i < movies.length; i++) {
+//         var movie = movies[i]
+//         // console.log(movie)
+//         var characters = movie.properties.characters
+//         // console.log(characters)
+//         for (var j = 0; j < characters.length; j++) {
+//             var characterUrl = characters[j];
+//             var characterNumber = characterUrl.split("/").pop();
+
+//             if (characterNumber === character_number) {
+//                 characterMovies.push(movie.properties.title);
+//                 console.log(characterMovies)
+//                 break;
+//             }
+//         }
+//     }
+//     var linkContainer = document.getElementById("link");
+//     linkContainer.innerHTML = ""; // Clear previous links
+    
+//     for (var k = 0; k < characterMovies.length; k++) {
+//         var movie = characterMovies[k];
+//         var link = document.createElement("a");
+//         link.href = "movie.html?character=" + encodeURIComponent(character_number);
+//         link.innerText = "View " + movie;
+//         linkContainer.appendChild(link);
+//         linkContainer.appendChild(document.createElement("br"));
+//     }
+// }
+
